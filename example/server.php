c5f4c1f3680f3f872d4e9f60d99651790547a982
inhere <in.798@qq.com>
2018-03-09 18:20:00 +0800

    up

--- example/server.php
+++ example/server.php
@@ -2,3 +2,34 @@
 
 $server = new SwooleLib\WebSocket\WebSocketServer;
 
+/*
+{
+    "_id" : ObjectId("5a46766fb3e4210e381e8212"),
+    "iStatus" : 2,
+    "iModelId" : 1099723,
+    "iStarLevel" : 5,
+    "sIdentityCardA" : "upload/head/1099723/f35af1af559711af299405ae2435cf94",
+    "sIdentityCardB" : "upload/head/1099723/583d829ffdd886bb6f905c2d0eb8d2b0",
+    "sRemark" : null,
+    "sWechatId" : "clarkwen",
+    "sSignature" : "孙悟空再牛屄也变不成美猴王",
+    "aHonor" : [
+        "胡子特别长"
+    ],
+    "aImpression" : [
+        16,
+        8,
+        14,
+        3
+    ],
+    "aBgPhotos" : [
+        "upload/head/1099723/d5a3808ae37a314990153fdbd03f3c9d"
+    ],
+    "aVideos" : [
+        "upload/video/1099723/c2aa6e7c85d1855993f1ac2570d3f686"
+    ],
+    "iCreateTime" : 1514567279,
+    "iUpdateTime" : 1515153861,
+    "iPassedTime" : 1515153861
+}
+*/
